c14ecf4371d231596efd4866f7e35455b573eb4e
Steve Clement <steve@localhost.lu>
2015-09-25 19:04:19 +0200

    Chapter 1 done and all the images added

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -9,13 +9,27 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
+
+  let zombie1 = SKSpriteNode(imageNamed: "zombie1")
+  
+  override func didMoveToView(view: SKView) {
+    backgroundColor = SKColor.whiteColor()
+    let background = SKSpriteNode(imageNamed: "background1")
+    //background.position = CGPoint(x: size.width/2, y: size.height/2)
+    background.anchorPoint = CGPointZero
+    background.position = CGPointZero
+    background.zPosition = -1
+    
+    zombie1.position = CGPoint(x: 400.0, y: 400.0)
+    addChild(background)
+    addChild(zombie1)
+
   }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
   }
    
-    override func update(currentTime: CFTimeInterval) {
+  override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-    }
+  }
 }
