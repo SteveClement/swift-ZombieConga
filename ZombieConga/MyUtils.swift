2dae7c24b2bc88943f033fd3c61e8bf3bd8197ab
Steve Clement <steve@localhost.lu>
2015-09-26 22:36:57 +0200

    Some explanatory comments

--- ZombieConga/MyUtils.swift
+++ ZombieConga/MyUtils.swift
@@ -9,18 +9,23 @@
 import Foundation
 import CoreGraphics
 
+// The following will extent the basic operations, +-*/ to be able to calculate vectors etc.
+// Before implementing this you would get an error because the (run-time) compiler wouldn't
+// know how to handle CGPoints for example. Now it has a reference on what to do
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
   return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
 func += (inout left: CGPoint, right: CGPoint) {
   left = left + right
 }
+
 func - (left: CGPoint, right: CGPoint) -> CGPoint {
   return CGPoint(x: left.x - right.x, y: left.y - left.x)
 }
 func -= (inout left: CGPoint, right: CGPoint) {
   left = left - right
 }
+
 func * (left: CGPoint, right: CGPoint) -> CGPoint {
   return CGPoint(x: left.x * right.x, y: left.y * left.x)
 }
@@ -45,6 +50,7 @@ func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
 func /= (inout point: CGPoint, scalar: CGFloat) {
   point = point / scalar
 }
+// This only gets considered if we are NOT (!) running on a 64bit Platform
 #if !(arch(x86_64) || arch(amd64))
   func atan2(y: CGFloat, x: CGFloat) -> CGFloat {
     return CGFloat(atan2f(Float(y), Float(x)))
@@ -54,6 +60,7 @@ func /= (inout point: CGPoint, scalar: CGFloat) {
   }
 #endif
 
+// This extent CGPoint assigned variables to have a .length()/.normalized()/.angle() property
 extension CGPoint {
   func length() -> CGFloat {
     return sqrt(pow(x, 2) + pow(y, 2))
