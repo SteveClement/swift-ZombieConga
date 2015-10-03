cdc0a453340433e4796fd893c7ab3ece1f1885a4
Steve Clement <steve@steves-13-inch-macbook-pro.local>
2015-10-03 08:52:25 +0200

    - indentation fix

--- ZombieConga/MyUtils.swift
+++ ZombieConga/MyUtils.swift
@@ -97,9 +97,9 @@ extension CGFloat {
 }
 
 extension CGFloat {
-    static func random() -> CGFloat{
-        return CGFloat(Float(arc4random()) / Float(UInt32.max))
-    }
+  static func random() -> CGFloat{
+    return CGFloat(Float(arc4random()) / Float(UInt32.max))
+  }
   static func random(min min: CGFloat, max: CGFloat) -> CGFloat {
     assert(min < max)
     return CGFloat.random() * (max - min) + min
